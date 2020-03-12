e2d8259c967ddc036a0887fb87c13cd779475436
Anthony Connor <ajc157@txstate.edu>
2020-03-11 23:19:58 -0500

    changes to gradle

--- junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
+++ junit5-jupiter-starter-gradle/src/test/java/com/example/project/CalculatorTests.java
@@ -26,6 +26,21 @@ class CalculatorTests {
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
 	}
 
+	//test for subtract method 
+	@Test
+	@DisplayName("51 - 49 = 2")
+	void subsTwoNumbers() {
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.sub(51, 49), "51 - 49 should equal 2");
+	}
+
+	@Test
+	@DisplayName("101 - 3 = 98")
+	void subsTwoNumbers() {
+		Calculator calculator2 = new Calculator();
+		assertEquals(98, calculator.sub(101, 3), "101 - 3 should equal 98`");
+	}
+
 	@ParameterizedTest(name = "{0} + {1} = {2}")
 	@CsvSource({
 			"0,    1,   1",
@@ -34,8 +49,15 @@ class CalculatorTests {
 			"1,  100, 101"
 	})
 	void add(int first, int second, int expectedResult) {
-		Calculator calculator = new Calculator();
+		Calculator calculator3 = new Calculator();
 		assertEquals(expectedResult, calculator.add(first, second),
 				() -> first + " + " + second + " should equal " + expectedResult);
 	}
+
+	void sub(int first, int second, int expectedResult) {
+		Calculator calculator = new Calculator();
+		assertEquals(expectedResult, calculator.subtract(first, second),
+				() -> first + " - " + second + " should equal " + expectedResult);
+	}
+
 }
